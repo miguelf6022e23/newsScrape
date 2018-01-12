4e652e1559b455b3273283be229c764d7d620c7f
miguelf6022e23 <miguelf6.022e23@gmail.com>
2018-01-12 00:25:34 -0500

    PORT!

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require('cheerio');
 // Require all models
 var db = require('./models');
 
-var PORT = 8080;
+var PORT = process.env.PORT;
 
 // Initialize Express
 var app = express();
